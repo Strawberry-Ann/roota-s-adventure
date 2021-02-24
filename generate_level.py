474aad0938806731697e528124190c30c5f5160c
Strawberry-Ann <maulann@yandex.ru>
2021-02-24 15:47:29 +0500

    Корректирована случайная генерация уровня

--- generate_level.py
+++ generate_level.py
@@ -1,4 +1,4 @@
-from random import randrange, choice
+from random import randrange, choice, choices
 
 
 class Level:
@@ -7,14 +7,19 @@ class Level:
         self.maxx, self.maxy = maxx, maxy
         self.sizex, self.sizey = sizex, sizey
         self.level = list()
+        self.add_sticks()
 
     def add_sticks(self):
-        for i in range(self.miny, self.sizey, self.maxy):
-            h = i - randrange(0, self.maxy - self.miny)
-            t = 1
-            for j in range(1, self.sizex // self.minx):
-                if choice(0, 1) == 1 or t == 1:
-                    w = j * (self.maxx) - randrange(0, self.maxx - self.minx)
-                    if t == 1:
-                        t = 0
-                    self.level.append((w, h))
+        c = 1
+        for i in range(self.sizey, self.miny, (self.miny // 2)):
+            if choice([0, 1]) == 1 or c == 1:
+                h = i - randrange(0, self.maxy - self.miny + 1)
+                m1 = randrange(1, self.sizex // self.minx)
+                m2, m3 = randrange(m1, (self.sizex // self.minx)), randrange(0, m1)
+                t = choices([0, 1, 2], k=2)
+                for k in [(0, m1), (1, m2), (2, m3)]:
+                    if k[0] in t:
+                        self.level.append((k[1] * self.minx, h))
+                c = 0
+            else:
+                c = 1
--- jumping.py
+++ jumping.py
@@ -1,5 +1,5 @@
 import pygame as pg
-from random import randrange
+from random import randrange, choices, choice
 from pygame import time
 import os
 import sys
@@ -86,6 +86,30 @@ class Background(pg.sprite.Sprite):
         self.rect = pg.Rect(0, 0, 350, 700)
 
 
+class Level:
+    def __init__(self, minx, miny, maxx, maxy, sizex, sizey):
+        self.minx, self.miny = minx, miny
+        self.maxx, self.maxy = maxx, maxy
+        self.sizex, self.sizey = sizex, sizey
+        self.level = list()
+        self.add_sticks()
+
+    def add_sticks(self):
+        c = 1
+        for i in range(self.sizey, self.miny, (self.miny // 2)):
+            if choice([0, 1]) == 1 or c == 1:
+                h = i - randrange(0, self.maxy - self.miny + 1)
+                m1 = randrange(1, self.sizex // self.minx)
+                m2, m3 = randrange(m1, (self.sizex // self.minx)), randrange(0, m1)
+                t = choices([0, 1, 2], k=2)
+                for k in [(0, m1), (1, m2), (2, m3)]:
+                    if k[0] in t:
+                        self.level.append((k[1] * self.minx, h))
+                c = 0
+            else:
+                c = 1
+
+
 if __name__ == '__main__':
     pg.init()
     pg.display.set_caption('Dodle Jump')
@@ -93,10 +117,13 @@ if __name__ == '__main__':
     screen = pg.display.set_mode(size)
     screen.fill((0, 0, 0))
     group_sprites = pg.sprite.Group()
+    Background()
     roota_sprites = pg.sprite.Group()
     sticks = pg.sprite.Group()
+    level = Level(20, 50, 50, 50, 350, -5000)
+    for x, y in level.level:
+        Stick(x, y)
     horizontal_borders = pg.sprite.Group()
-    Background()
     hero = Roota(100, 600)
     Border(5, height - 5, width - 5, height - 5)
     running = True
