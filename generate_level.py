3a15f3a52c12b9309caa27fc915b89625061093e
Strawberry-Ann <maulann@yandex.ru>
2021-02-24 15:57:11 +0500

    Ещё раз корректирована случайная генерация уровня

--- generate_level.py
+++ generate_level.py
@@ -14,7 +14,7 @@ class Level:
         for i in range(self.sizey, self.miny, (self.miny // 2)):
             if choice([0, 1]) == 1 or c == 1:
                 h = i - randrange(0, self.maxy - self.miny + 1)
-                m1 = randrange(1, self.sizex // self.minx)
+                m1 = randrange(1, self.sizex // self.minx - 1)
                 m2, m3 = randrange(m1, (self.sizex // self.minx)), randrange(0, m1)
                 t = choices([0, 1, 2], k=2)
                 for k in [(0, m1), (1, m2), (2, m3)]:
